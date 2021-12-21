8d8b9fd911d289529d546aab5786ce9650d3cade
Jude Klevine <77893132+JudeKLevine@users.noreply.github.com>
2021-12-21 19:06:27 +0100

    Create Defi Turing40.py

---
+++ Defi Turing40.py
@@ -0,0 +1,4 @@
+A = [str(i) for i in range(30000000)]
+A = "".join(A)
+print(int(A[1])*int(A[10])*int(A[100])*int(A[1000])*int(A[10000])*int(A[100000])*int(A[1000000])*int(A[10000000])*int(A[100000000]))
+# 11760
